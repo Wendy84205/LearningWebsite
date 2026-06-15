7661dae75de195af182c11a6c73cf499ac57fed8
wendy <khanhdinh323@gmail.com>
2026-06-16 00:23:42 +0700

    fix: add missing useCallback import to parent-login page

--- src/app/parent-login/page.js
+++ src/app/parent-login/page.js
@@ -1,5 +1,5 @@
 'use client'
-import { useState, useEffect, Suspense } from 'react'
+import { useState, useEffect, useCallback, Suspense } from 'react'
 import { useRouter, useSearchParams } from 'next/navigation'
 import Link from 'next/link'
 import styles from './page.module.css'
