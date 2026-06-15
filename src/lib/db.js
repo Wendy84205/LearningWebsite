2c9904eb6053d66cb856833653488e8d252ffff2
wendy <wendy84205@gmail.com>
2026-06-15 18:43:18 +0700

    fix: remove SQLite adapter, use plain PrismaClient for PostgreSQL

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,14 +1,9 @@
 import { PrismaClient } from '@/generated/prisma/client'
-import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3'
-import path from 'path'
-
-const dbPath = path.join(process.cwd(), 'dev.db')
 
 const globalForPrisma = globalThis
 
 if (!globalForPrisma.prisma) {
-  const adapter = new PrismaBetterSqlite3({ url: `file:${dbPath}` })
-  globalForPrisma.prisma = new PrismaClient({ adapter })
+  globalForPrisma.prisma = new PrismaClient()
 }
 
 const prisma = globalForPrisma.prisma
